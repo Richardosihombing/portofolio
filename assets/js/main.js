340b53be37ece910c0cb5e0ac526de8cbcf76339
richardo sihombing <109953036+Richardosihombing@users.noreply.github.com>
2023-08-29 11:26:48 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,7 +30,13 @@ const linkAction = () =>{
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== SHADOW HEADER ===============*/
-
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('shadow-header') 
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== EMAIL JS ===============*/
 
